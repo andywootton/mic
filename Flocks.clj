c51ea27b70a9230df1b74c2a5cefb04f9b92acdc
Andy Wootton <andy.wootton@bcs.org.ukgit config --global user.name Andy>
2017-06-16 19:24:33 +0100

    Some people and some flock names

---
+++ Flocks.clj
@@ -0,0 +1,47 @@
+People
+
+BBQ
+
+Silicon Canal
+(Simon Jenner) 
+
+383 Project
+(Kath Preston)
+
+Meducation
+(Jeremy Walker)
+
+Talis
+(@ChrisC
+ @pads Ben
+ Roman
+ Richard)
+
+@dnews Daniel News
+
+@stevejalim Steve Jalim
+@auxbuss Marc Cooper
+
+? @rythie Richard Cunningham
+? @joeturner Joe Turner
+@robstyles Rob Styles
+
+Manning Publications
+@gregjwild Greg Wild
+
+brumtech Slack /mic
+@alexrussell
+@limeblast Daniel Hollands
+brumtech: @jimseconde b.io: @jims
+@sil Stuart Langridge
+@woo Andy Wootton
+
+Flocks
+
+@Silicon_Canal 923
+@Multipack 644 (117 in common with @Silicon_Canal)
+@Techwednesday 776
+@HydraHack 498 (155 in common with @Silicon_Canal)
+"So I (now) think there are more than 1000 people" - Rythie
+
+birmingham.io 590
